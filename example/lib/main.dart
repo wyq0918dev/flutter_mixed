b51fca94d27ac2f80ef275f81a442d3f7f5130e5
wyq0918dev <wyq0918dev@gmail.com>
2025-08-21 12:38:04 +0800

    commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,22 +3,7 @@ import 'package:flutter_mixed/flutter_mixed.dart';
 import 'package:freefeos/freefeos.dart';
 import 'package:multi_builder/multi_builder.dart';
 
-void main() => MyWidget()();
-
-abstract base class RunableWidget extends StatelessWidget {
-  const RunableWidget({super.key});
-
-  void call() => runApp(this);
-}
-
-final class MyWidget extends RunableWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MyApp();
-  }
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
