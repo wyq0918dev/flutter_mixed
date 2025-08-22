d60c60f4164985d11ad603bc32b7f031b2ffb02e
wyq0918dev <wyq0918dev@gmail.com>
2025-08-22 13:51:01 +0800

    commit

--- android/src/main/kotlin/com/wyq0918dev/flutter_mixed/FlutterMixedUtils.kt
+++ android/src/main/kotlin/com/wyq0918dev/flutter_mixed/FlutterMixedUtils.kt
@@ -3,11 +3,22 @@ package com.wyq0918dev.flutter_mixed
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.size
+import com.wyq0918dev.flutter_mixed.FlutterMixed.Companion.ENGINE_ID
 import io.flutter.embedding.android.FlutterView
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 
 internal object FlutterMixedUtils {
+
+    /**
+     * 检查引擎是否已初始化
+     *
+     * @return true 未初始化 false 已初始化
+     */
+    internal fun checkEngineInitialize(): Boolean {
+        return FlutterEngineCache.getInstance().get(ENGINE_ID) == null
+    }
 
     internal fun findFlutterView(view: View?): FlutterView? {
         when (view) {
